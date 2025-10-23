7697c71bd2cdf321f0b0dca58bf74f62cbb1f213
IgorNatann <igornatan4@gmail.com>
2025-10-23 16:11:10 -0300

    feat: adicionar funções para renomear e obter colunas dos dados

--- scripts/fusao_mercado_out.py
+++ scripts/fusao_mercado_out.py
@@ -24,12 +24,42 @@ def leitura_dados(path, tipo_arquivo):
         dados = leitura_json(path)
     return dados
 
+def get_columns(dados):
+    return list(dados[0].keys())
+
+def rename_columns(dados, key_mapping):
+    new_dados_csv = []
+
+    for old_dict in dados:
+        dict_temp = {}
+        for old_key, value in old_dict.items():
+            dict_temp[key_mapping[old_key]] = value
+        new_dados_csv.append(dict_temp)
+    return new_dados_csv
+
 path_json = '/home/pipeline-dados/Documentos/pipeline_dados/data_raw/dados_empresaA.json'
 
 path_csv = '/home/pipeline-dados/Documentos/pipeline_dados/data_raw/dados_empresaB.csv'
 
+# Iniciando a leitura
 dados_json = leitura_dados(path_json, 'json')
-print(dados_json[0])
+nome_colunas_json = get_columns(dados_json)
+print(f"Nome colunas dados Json: {nome_colunas_json}")
 
 dados_csv = leitura_dados(path_csv, 'csv')
-print(dados_csv[0])
+nome_colunas_csv = get_columns(dados_csv)
+print(f"Nome colunas dados Csv: {nome_colunas_csv}")
+
+key_mapping = {'Nome do Item': 'Nome do Produto',
+               'Classificação do Produto': 'Categoria do Produto',
+               'Valor em Reais (R$)': 'Preço do Produto',
+               'Quantidade em Estoque': 'Quantidade em Estoque',
+               'Nome da Loja': 'Filial',
+                'Data da Venda': 'Data da Venda'
+              }
+
+# Transformação dos dados
+
+dados_csv = rename_columns(dados_csv, key_mapping)
+nome_colunas_csv = get_columns(dados_csv)
+print(f"Colunas transformadas: {nome_colunas_csv}")
